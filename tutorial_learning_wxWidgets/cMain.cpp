4dad287a4c869dec45690c40b7f15c3f73641110
László-zsolt Szabadi <42877326+Pliexe@users.noreply.github.com>
2021-08-19 23:15:48 +0200

    wadaw

--- tutorial_learning_wxWidgets/cMain.cpp
+++ tutorial_learning_wxWidgets/cMain.cpp
@@ -1,8 +1,12 @@
 #include "cMain.h"
 
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+	EVT_BUTTON(1001, OnButtonClicked)
+wxEND_EVENT_TABLE()
+
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "First GUI c++ app I made in wxWidgets!", wxPoint(30, 30), wxSize(800, 600))
 {
-	m_btn1 = new wxButton(this, wxID_ANY, "Click Me", wxPoint(10, 10), wxSize(150, 50));
+	m_btn1 = new wxButton(this, 1001, "Click Me", wxPoint(10, 10), wxSize(150, 50));
 	m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
 	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
 }
@@ -10,4 +14,10 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "First GUI c++ app I made in wxWidge
 cMain::~cMain()
 {
 
+}
+
+void cMain::OnButtonClicked(wxCommandEvent& evt)
+{
+	m_list1->AppendString(m_txt1->GetValue());
+	evt.Skip();
 }
